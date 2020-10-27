3e0b697a74992c7f75607a54bdf5ce93acd4fd3e
fenghua-mly <1184119358@qq.com>
2020-10-27 19:08:24 +0800

    第二天练习

--- assets/js/login.js
+++ assets/js/login.js
@@ -6,7 +6,42 @@ $(function() {
         })
         // 点击登录，隐藏注册，显示登录
     $('#link-login').on('click', function() {
-        $('.login-box').show()
-        $('.reg-box').hide()
+            $('.login-box').show()
+            $('.reg-box').hide()
+        })
+        //自定义校验规则
+    var form = layui.form
+    form.verify({
+            pwd: [
+                /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+            ],
+            repwd: function(value) {
+                var pwd = $('.reg-box [name=password]').val()
+                if (value !== pwd) {
+                    return '两次密码不一致，请重新输入'
+                }
+            }
+        })
+        // 监听表单注册事件
+    $('#form-reg').on('submit', function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "http://ajax.frontend.itheima.net/api/reguser",
+            data: {
+                username: $('#form-reg [name=username]').val(),
+                password: $('#form-reg [name=password]').val(),
+            },
+            success: function(res) {
+                if (res.status !== 0) {
+                    return alert(res.message)
+                }
+                alert(res.message)
+            }
+
+        })
     })
+
+
+
 })
