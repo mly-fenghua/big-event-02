78e66a0ae677f676583aebf11758d4157f31dc92
fenghua-mly <1184119358@qq.com>
2020-10-30 19:03:16 +0800

    头像上传

--- assets/js/user-avatar.js
+++ assets/js/user-avatar.js
@@ -9,3 +9,50 @@ const options = {
     }
     // 1.3 创建裁剪区域 
 $image.cropper(options)
+
+// 2.绑定上传事件按钮
+$('#btnChooseImage').on('click', function() {
+    $('#file').click()
+})
+var layer = layui.layer
+    // 3.修改图片
+$('#file').on('change', function(e) {
+    // 1. 拿到用户选择的文件
+    var file = e.target.files[0]
+    if (file == undefined) {
+        return layer.msg('请选择图片!')
+    }
+    // 2. 将文件，转化为路径
+    var imgURL = URL.createObjectURL(file)
+        // 3. 重新初始化裁剪区域
+    $image
+        .cropper('destroy') // 销毁旧的裁剪区域
+        .attr('src', imgURL) // 重新设置图片路径
+        .cropper(options) // 重新初始化裁剪区域
+})
+$('#btnUpload').on('click', function() {
+    var dataURL = $image
+        .cropper('getCroppedCanvas', {
+            // 创建一个 Canvas 画布
+            width: 100,
+            height: 100
+        })
+        .toDataURL('image/png')
+    $.ajax({
+        url: '/my/update/avatar',
+        type: 'POST',
+        data: { avatar: dataURL },
+        success: function(res) {
+            if (res.status !== 0) {
+                return layer.msg('更换头像失败！')
+
+            }
+            layer.msg('更换头像成功！')
+
+            window.parent.getUserInfo()
+
+
+        }
+    })
+
+})
