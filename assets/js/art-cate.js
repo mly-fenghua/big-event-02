4d4c67642fcb97b2f83e3274381f05a219f937d7
fenghua-mly <1184119358@qq.com>
2020-11-01 21:11:31 +0800

    qq

--- assets/js/art-cate.js
+++ assets/js/art-cate.js
@@ -1,20 +1,18 @@
 $(function() {
     var layer = layui.layer
-    var form = layui.form
-        //1.获取文章分类列表
     initArtCateList()
 
     function initArtCateList() {
         $.ajax({
-            type: 'GET',
             url: '/my/article/cates',
+            method: 'GET',
             success: function(res) {
                 var htmlStr = template('tpl-table', res)
                 $('tbody').html(htmlStr)
             }
         })
     }
-    //2. 添加分类事件
+    // 添加分类事件
     $('#btnAddCate').on('click', function() {
             indexAdd = layer.open({
                 type: 1,
@@ -22,9 +20,8 @@ $(function() {
                 title: '添加文章分类',
                 content: $('#dialog-add').html()
             });
-
         })
-        //3. 添加文章分类
+        // 添加文章分类
     var indexAdd = null
     $('body').on('submit', '#form-add', function(e) {
 
@@ -45,16 +42,17 @@ $(function() {
 
             })
         })
-        // 4.修改文章分类，编辑按钮
+        // 修改文章分类
+    var form = layui.form
+        // 绑定btn-edit事件
     var indexEdit = null
-    $('body').on('click', '.btn-edit', function(e) {
+    $('body').on('click', '.btn-edit', function() {
+
             indexEdit = layer.open({
                 type: 1,
                 area: ['500px', '250px'],
-                title: '编辑文章分类',
+                title: '修改文章分类',
                 content: $('#dialog-edit').html()
-
-
             });
             // 显示完毕修改form， 在获取Id对应文章分类信息
             var id = $(this).attr("data-id")
@@ -66,29 +64,30 @@ $(function() {
                 }
             })
         })
-        // 5.修改表单分类事件
+        // 修改表单分类事件
         // 添加文章分类
     var indexEdit = null
     $('body').on('submit', '#form-edit', function(e) {
 
-            e.preventDefault();
-            $.ajax({
-                method: "POST",
-                url: '/my/article/updatecate',
-                data: $(this).serialize(),
-                success: function(res) {
-                    if (res.status !== 0) {
-                        return layer.msg('更新分类失败!')
-                    }
-                    // 刷新页面
-                    initArtCateList()
-                    layer.msg('更新分类成功!')
-                    layer.close(indexEdit)
+        e.preventDefault();
+        $.ajax({
+            method: "POST",
+            url: '/my/article/updatecate',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新分类失败!')
                 }
+                // 刷新页面
+                initArtCateList()
+                layer.msg('更新分类成功!')
+                layer.close(indexEdit)
+            }
 
-            })
         })
-        //6. 删除按钮
+    })
+
+    // 删除按钮
     $('body').on('click', '.btn-delete', function() {
         var Id = $(this).attr("data-id")
         layer.confirm('确定删除吗?', { icon: 3, title: '提示' }, function(index) {
--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,12 +1,19 @@
 // 开发环境地址
-var baseUrl = 'http://ajax.frontend.itheima.net'
+var baesUrl = "http://ajax.frontend.itheima.net"
     // 测试环境地址
-    // var baseUrl = 'http://ajax.frontend.itheima.net'
-    // 运行环境地址
-    // var baseUrl = 'http://ajax.frontend.itheima.net'
+    // var baesUrl = "http://ajax.frontend.itheima.net"
+    // 生产环境地址
+    // var baesUrl = "http://ajax.frontend.itheima.net"
+
+
+
+// $.ajaxPrefilter()绑定在ajax之前,并在ajax请求之后触发，最后ajax才真正发送。
 $.ajaxPrefilter(function(options) {
-    // 拼接地址
-    options.url = baseUrl + options.url
+    // console.log(options.url);
+    options.url = baesUrl + options.url
+
+
+    // 统一带my添加请求头
     if (options.url.indexOf('/my/') !== -1) {
         options.headers = {
             Authorization: localStorage.getItem("token") || ''
@@ -14,11 +21,16 @@ $.ajaxPrefilter(function(options) {
     }
     // 登录拦截
     options.complete = function(res) {
+
         // console.log(res);
-        if (res.responseJSON.status === 1 && res.responseJSON.message === "登录失败！") {
-            localStorage.removeItem('token')
+        // console.log(res.responseJSON);
+
+
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            localStorage.removeItem("token");
             location.href = "/login.html"
         }
-
     }
+
+
 })
